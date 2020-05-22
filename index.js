d3a5700c5598ef4bfb4830c94094f35042ee3961
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-22 20:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
